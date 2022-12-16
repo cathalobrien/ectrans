e002a9c67bd6c960e68e33181d62e546d7430f73
Cathal OBrien <cathal.obrien@ecmwf.int>
2022-12-16 09:41:59 +0000

    simplfied env scripts

--- arch/atos/gnu_openmpi/env.sh
+++ arch/atos/gnu_openmpi/env.sh
@@ -1,2 +1,7 @@
 module load prgenv/gnu 
 module load openmpi
+
+#common across all compilers and mpi implementations
+module load cmake
+module load ecbuild
+module load intel-mkl 
--- arch/atos/intel_intelmpi/env.sh
+++ arch/atos/intel_intelmpi/env.sh
@@ -1,2 +1,7 @@
 module load prgenv/intel 
 module load intel-mpi
+
+#common across all compilers and mpi implementations
+module load cmake
+module load ecbuild
+module load intel-mkl 
